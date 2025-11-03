19116f12395c6dbbd1aa46c3468a74ee6069af43
Naili Lucia <132862882+nah5f4@users.noreply.github.com>
2025-11-03 17:36:13 -0300

    Create solucoes.py

---
+++ solucoes.py
@@ -0,0 +1,11 @@
+def sao_anagramas(string1, string2):
+  # Todo: Implementar a lógica
+  pass
+
+def cifra_de_cesar(texto, deslocamento): 
+  # Todo: Implementar a lógica
+  pass
+
+def valida_cpf(cpf_string):
+  # Todo: Implementar a lógica
+  pass
