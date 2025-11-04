94ee3ac397cd7dca7eeaa033f6c227a774b09d92
Rachel <rachelloriato@gmail.com>
2025-11-04 19:15:22 -0300

    feat: implementa logica da funcao anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,8 +1,23 @@
 # Soluções:
 
 def sao_anagramas(string1, string2):
-  # Todo: Implementar a lógica
-  pass
+  count = [0] * 26
+  for char in string1:
+    if(ord(char) >= ord('a') and ord(char) <= ord('z')):
+      count[ord(char) - ord('a')] += 1
+    elif(ord(char) >= ord('A') and ord(char) <= ord('Z')):
+      count[ord(char) - ord('A')] += 1
+
+  for char in string2:
+    if(ord(char) >= ord('a') and ord(char) <= ord('z')):
+      count[ord(char) - ord('a')] -= 1
+    elif(ord(char) >= ord('A') and ord(char) <= ord('Z')):
+      count[ord(char) - ord('A')] -= 1
+
+  for c in count:
+    if c != 0:
+      return False
+  return True
 
 def cifra_de_cesar(texto, deslocamento): 
   # Todo: Implementar a lógica
