272d172ecdc1ec0be18c52a25b1af95d9edfe07b
Giovanna Luisa Hirata dos Anjos <giovannaluisahirata@gmail.com>
2025-11-04 09:14:41 -0300

    feat: implementa logica da funcao de cifra_de_cesar

--- solucoes.py
+++ solucoes.py
@@ -6,7 +6,23 @@ def sao_anagramas(string1, string2):
 
 def cifra_de_cesar(texto, deslocamento): 
   # Todo: Implementar a lógica
-  pass
+  #pass
+  """
+  [A-Z]: [65-90]
+  [a-z]: [97-122]
+  """
+  
+  cifra = ""
+  for caractere in texto:
+    if caractere.isalpha():
+      char_val = ord(caractere)
+      if char_val < 97:
+        cifra += chr((char_val - 65 + deslocamento) % 26 + 65)
+      else:
+        cifra += chr((char_val - 97 + deslocamento) % 26 + 97)
+    else:
+      cifra += caractere
+  return cifra
 
 def valida_cpf(cpf_string):
   # Todo: Implementar a lógica
