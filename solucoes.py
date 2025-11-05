231d72c0d01563bbc5d02b35a992d135a202cbde
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-04 21:16:36 -0300

    feat: implementa logica da funcao encontrar maior palavra

--- solucoes.py
+++ solucoes.py
@@ -8,6 +8,16 @@ def cifra_de_cesar(texto, deslocamento):
   # Todo: Implementar a lógica
   pass
 
-def encontrar_maior_palavra(frase):
-  # Todo: Implementar a lógica
-  pass
+
+def encontrar_maior_palavra(frase: str) -> str:
+    maio_palavra = ""
+    frase_sem_pontuacao = ""
+    for caractere in frase:
+        if (caractere.lower() >= "a" and caractere.lower() <= "z") or caractere == " ":
+            frase_sem_pontuacao += caractere  # ignora caracteres que não são letras
+
+    palavras = frase_sem_pontuacao.split()  # separa palavras por espaços
+    for palavra in palavras:
+        if len(palavra) > len(maio_palavra):
+            maio_palavra = palavra
+    return maio_palavra
