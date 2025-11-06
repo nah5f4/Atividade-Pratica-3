ae968d5f96ba0e4d23e41b8ff3a0d6c5edd0b36f
Rachel <rachelloriato@gmail.com>
2025-11-06 18:29:00 -0300

    Implementacao da funcao sao_anagramas corrigida

--- solucoes.py
+++ solucoes.py
@@ -1,21 +1,25 @@
 # Soluções:
 
 def sao_anagramas(string1, string2):
-  count = [0] * 26
+  dicionario = {}
   for char in string1:
-    if(ord(char) >= ord('a') and ord(char) <= ord('z')):
-      count[ord(char) - ord('a')] += 1
-    elif(ord(char) >= ord('A') and ord(char) <= ord('Z')):
-      count[ord(char) - ord('A')] += 1
+    if ord(char) == ord(' '):
+      continue
+    if ord(char) >= ord('A') and ord(char) <= ord('Z'):
+      dicionario[ord(char) + 32] = dicionario.get(ord(char) + 32, 0) + 1
+    else:
+      dicionario[ord(char)] = dicionario.get(ord(char), 0) + 1
 
   for char in string2:
-    if(ord(char) >= ord('a') and ord(char) <= ord('z')):
-      count[ord(char) - ord('a')] -= 1
-    elif(ord(char) >= ord('A') and ord(char) <= ord('Z')):
-      count[ord(char) - ord('A')] -= 1
+    if ord(char) == ord(' '):
+      continue
+    if ord(char) >= ord('A') and ord(char) <= ord('Z'):
+      dicionario[ord(char) + 32] = dicionario.get(ord(char) + 32, 0) - 1
+    else:
+      dicionario[ord(char)] = dicionario.get(ord(char), 0) - 1
 
-  for c in count:
-    if c != 0:
+  for quantidade in dicionario.values():
+    if quantidade != 0:
       return False
   return True
 
